b859eefdbaf6b1d8897a822daf68e926babc59b8
Donara08 <donara.amiryan@tumo.org>
2023-04-08 12:40:58 +0400

    JS class inneriance 2

--- gameOfLife 3/fire.js
+++ gameOfLife 3/fire.js
@@ -1,7 +1,6 @@
-class Fire {
+class Fire extends LivingCreature {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        super(x,y)
         this.energy = 5
         this.directions = []
     }
